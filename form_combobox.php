937d816cb400db6a92593d606a3cfec9942eb08d
Gopal <valnovcc@gmail.com>
2018-10-03 20:28:41 +0700

    Form Combobox sederhana PHP

---
+++ form_combobox.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nama negara dan benuanya</title>
+</head>
+<body>
+    <?php
+    //koneksi ke database
+    $con = mysqli_connect("localhost","sapa","melbu","trik");
+    $query = "SELECT * FROM countries";
+    $sql = mysqli_query($con, $query);
+    ?>
+    <form action="" method="POST">
+        <select name="negara">
+            <?php
+            while ($data = mysqli_fetch_array($sql)) {
+                echo '<option value="$data[id]">'.$data['negara'].'</option>';
+            }
+            ?>
+        </select>
+
+        <input type="submit" value="Go">
+    </form>
+</body>
+</html>
